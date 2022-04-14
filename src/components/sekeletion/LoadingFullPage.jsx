2cb26b18016f956120e7da2dafa1f474ab555d13
Tuan Nguyen <nguyenanhtuan0799@gmail.com>
2022-04-15 00:21:10 +0700

    fix loading login

--- src/components/sekeletion/LoadingFullPage.jsx
+++ src/components/sekeletion/LoadingFullPage.jsx
@@ -6,7 +6,9 @@ function LoadingFullPage() {
     <div className="container">
       <div className="overlay"></div>
       <div className="content">
-        <img className="img" src={img} alt="loading" />
+        <div class="lds-circle">
+          <div></div>
+        </div>
       </div>
     </div>
   );
